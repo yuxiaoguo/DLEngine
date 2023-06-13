cef21177e0dc1b0603991038c57be54fede79d29
Yuxiao Guo <yuxiao.guo@outlook.com>
2023-06-13 16:25:49 +0800

    Using data class (#6)
    Co-authored-by: Yuxiao Guo <yuxiao.guo@outlook.com>

---
+++ dl_engine/data/protocols/__init__.py
@@ -0,0 +1,4 @@
+"""
+Copyright (c) 2023 Yu-Xiao Guo All rights reserved.
+"""
+from .sequential_data import SequentialDataDescV0, MetaSeqFileDescV0
---
+++ dl_engine/data/protocols/sequential_data.py
@@ -0,0 +1,45 @@
+"""
+Copyright (c) 2023 Yu-Xiao Guo All rights reserved.
+"""
+from dataclasses import dataclass, field
+
+
+@dataclass
+class MetaFileDescV0:
+    """Meta File Protocol Version 0
+
+    Attributes:
+        meta_name: str, the name of meta file without extension
+    """
+    meta_file: str = ''
+    num_samples: int = 0
+    global_offset: int = 0
+
+
+@dataclass
+class MetaSeqFileDescV0(MetaFileDescV0):
+    """Meta Sequential File Protocol Version 0
+
+    Attributes:
+        meta_name: str, the name of meta file without extension
+    """
+    num_nonseq_samples: int = 0
+    global_nonseq_offset: int = 0
+    local_nonseq_offset: list[int] = field(default_factory=list)
+
+
+@dataclass
+class SequentialDataDescV0:
+    """Sequential Data Protocol Version 0
+
+    Attributes:
+        package_type: str, 'zip', 'pkl' or 'tar'
+    """
+    package_type: str = 'zip'
+    meta_files: list[MetaSeqFileDescV0] = field(default_factory=list)
+    total_samples: int = 0
+    total_nonseq_samples: int = 0
+    props: dict = field(default_factory=dict)
+
+    def __post_init__(self):
+        self.meta_files = [MetaSeqFileDescV0(**_meta_file) for _meta_file in self.meta_files]
