4428ee867f88be2034c946d7ce13170d61833436
Yuxiao Guo <yuxgu@microsoft.com>
2023-06-15 07:58:17 +0800

    Start to support concat dataset

--- dl_engine/data/__init__.py
+++ dl_engine/data/__init__.py
@@ -4,3 +4,4 @@ Copyright (c) 2023 Yu-Xiao Guo All rights reserved.
 from .samplers.file_storage_sampler import FileStorageSampler
 
 from .datasets.partial_lfs_dataset import PartialLFSDataset
+from .datasets.concat_lfs_dataset import ConcatLFSDataset
