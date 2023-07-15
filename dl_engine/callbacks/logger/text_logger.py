c7a2353ec6ce4efec00cd696fcddd1cb2ac9af4b
Yuxiao Guo <yuxiao.guo@outlook.com>
2023-07-15 16:32:00 +0800

    Text logger will separately show losses and metrics now. (#32)

--- dl_engine/callbacks/logger/text_logger.py
+++ dl_engine/callbacks/logger/text_logger.py
@@ -19,24 +19,30 @@ class ProgressTextLogger(BaseCallback):
         self._stride = stride
         self._iter = 0
 
-        self._step_recorder: Dict[str, List] = dict()
+        self._loss_recorder: Dict[str, List] = dict()
+        self._metric_recorder: Dict[str, List] = dict()
 
     def _gather_iter_info(self, iter_out: BaseIO):
         for l_name, l_tenor in iter_out.losses.items():
-            self._step_recorder.setdefault(l_name, list()).append(l_tenor.item())
+            self._loss_recorder.setdefault(l_name, list()).append(l_tenor.item())
         for m_name, m_tenor in iter_out.metrics.items():
-            self._step_recorder.setdefault(m_name, list()).append(m_tenor.item())
+            self._metric_recorder.setdefault(m_name, list()).append(m_tenor.item())
 
     def _gen_out_str(self):
-        out_str = ' - '.join(
-            [f'{k}: {sum(v) / len(v):.5f}' for k, v in self._step_recorder.items()])
-        return out_str
+        out_loss_str = ' - '.join(
+            [f'{k}: {sum(v) / len(v):.7f}' for k, v in self._loss_recorder.items()])
+        out_loss_str = f'Losses: {out_loss_str}'
+        out_metric_str = ' - '.join(
+            [f'{k}: {sum(v) / len(v):.7f}' for k, v in self._metric_recorder.items()])
+        out_metric_str = f'Metrics: {out_metric_str}'
+        return out_loss_str, out_metric_str
 
     def reset(self) -> None:
         self._iter = 0
 
     def close(self) -> None:
-        self._step_recorder = dict()
+        self._loss_recorder = dict()
+        self._metric_recorder = dict()
 
     def _run(self, io_proto, **extra_kwargs):
         """
@@ -44,6 +50,7 @@ class ProgressTextLogger(BaseCallback):
         """
         self._gather_iter_info(io_proto)
         if self._iter % self._stride == 0:
-            out_msg = self._gen_out_str()
-            Logger().info_zero_rank(f'{self._prefix} (iter: {self._iter:07d}) - {out_msg}')
+            out_loss_msg, out_metric_msg = self._gen_out_str()
+            Logger().info_zero_rank(f'{self._prefix} (iter: {self._iter:07d}) - {out_loss_msg}')
+            Logger().info_zero_rank(f'{self._prefix} (iter: {self._iter:07d}) - {out_metric_msg}')
         self._iter += 1
