2e708a9068197aea288c7250274ed5fff844776d
Yuxiao Guo <yuxiao.guo@outlook.com>
2024-01-31 12:15:41 +0800

    Update setup.py to support higher versions

--- setup.py
+++ setup.py
@@ -13,17 +13,17 @@ setup(
     license='MIT',
     packages=find_packages(),
     install_requires=[
-        'torch==1.13.1',
+        'torch>=1.13.1',
         'PyYAML==6.0',
         'tqdm==4.65.0',
-        'transformers==4.27.4',
+        'transformers>=4.27.4',
         'tensorboard==2.12.2',
         'configargparse==1.5.3',
         'opencv-python-headless',
         'librosa==0.10.0.post2',
-        'lightning==2.0.2',
+        'lightning>=2.0.2',
         'moviepy==1.0.3',
-        'torchmetrics==0.11.4',
+        'torchmetrics>=0.11.4',
         'gitpython'
     ],
     zip_safe=False
